4be0b55cc490c81b53800541f09dfcb65376890b
Kirillgutsalo <kirya.sever11@gmail.com>
2021-12-01 18:25:18 +0200

    hw#16 by Yuriy

--- src/telran/util/memory/MemoryService.java
+++ src/telran/util/memory/MemoryService.java
@@ -2,26 +2,21 @@ package telran.util.memory;
 
 public class MemoryService {
 public static int getMaxAvailableMemory() {
-	long maxMemory = Runtime.getRuntime().maxMemory();
-	int maxSize = maxMemory > Integer.MAX_VALUE ? Integer.MAX_VALUE : (int) maxMemory;
 	
-	int left = 0;
-	int right = maxSize;
-	int middle = 0;
-	int maxAvailableMemory = 0;
+	int maxSize = Integer.MAX_VALUE;
+	int left = 0, right = maxSize, middle = 0;
 	byte ar[] = null;
-	
 	while (left <= right) {
-		middle = (int)(((long)right + left) / 2);
+		middle = (int)(((long)right + left) / 2) ;
 		try {
-			ar = new byte[middle];
-			maxAvailableMemory = middle;
-			left = middle++;
-			ar = null;
-		} catch (OutOfMemoryError e) {
-					right = middle--;
-					}
+			 ar = new byte[middle];
+			 ar = null;
+			maxSize = middle;
+			left = middle + 1;
+		} catch (Throwable e) {
+			right = middle - 1;
 		}
-	return maxAvailableMemory;
+	}
+	return maxSize;
 }
 }
